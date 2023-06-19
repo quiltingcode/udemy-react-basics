62d3f791abcda37d5247206ba6dba97ca7ce49f9
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-06-19 14:01:28 +0000

    Add filter and includes for search bar

--- character-app/src/App.js
+++ character-app/src/App.js
@@ -2,7 +2,9 @@ import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  
   constructor() {
+    console.log('constructor')
     super();
     this.state = {
       characters: [],
@@ -10,6 +12,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    console.log('componentDidMount')
     fetch("https://api.disneyapi.dev/character")
       .then((response) => response.json())
       .then((users) =>
@@ -22,11 +25,29 @@ class App extends Component {
           }
         )
       );
-  }
+    }
 
   render() {
+    console.log('render')
     return (
       <div className="App">
+        <input 
+          className="search-box" 
+          type='search' 
+          placeholder='search characters' 
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLowerCase();
+            const filteredCharacters = this.state.characters.filter((character) => {
+              return character.name.toLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { characters: filteredCharacters };
+            })
+          }} 
+        />
+        
         {this.state.characters.map((character) => {
           return (
             <div key={character._id}>
