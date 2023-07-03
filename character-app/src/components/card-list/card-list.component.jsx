ade426a7699c2af1a3df08dc512691930203a88b
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-03 09:11:09 +0000

    Make Card-list functional component

--- character-app/src/components/card-list/card-list.component.jsx
+++ character-app/src/components/card-list/card-list.component.jsx
@@ -1,21 +1,14 @@
-import { Component } from 'react';
 import '../card-list/card-list.style.css'
 import Card from './card.component';
 
-class CardList extends Component {
-    render() {
-        const { characters } = this.props
-        return (
-          <div className="card-list">
-            {characters.map((character) => {
-              return (
-                <Card character={character} key={character._id}/>
-              );
-            })}
-          </div>
-        );
-    }
-}
+const CardList = ({ characters }) => (
+    <div className="card-list">
+      {characters.map((character) => {
+        return <Card character={character} key={character._id} />;
+      })}
+    </div>
+  );
+
 
 
 export default CardList;
