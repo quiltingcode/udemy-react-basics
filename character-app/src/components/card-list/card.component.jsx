b76d31efca744ff0026239dec3a6a35b2525d142
Kelly Hutchison <106617070+quiltingcode@users.noreply.github.com>
2023-07-03 09:17:44 +0000

    Convert searchbox and card to functional components

--- character-app/src/components/card-list/card.component.jsx
+++ character-app/src/components/card-list/card.component.jsx
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Card extends Component {
-  render() {
-
-    const { name, imageUrl, _id, tvShows } = this.props.character;
-    return (
-      <div className="card-container" key={_id}>
-        <img alt={`disney character ${name}`} src={imageUrl} />
-        <h2>{name}</h2>
-        <p>{tvShows}</p>
-      </div>
-    );
-  }
-}
+const Card = (props) => {
+  const { name, imageUrl, _id, tvShows } = props.character;
+  return (
+    <div className="card-container" key={_id}>
+      <img alt={`disney character ${name}`} src={imageUrl} />
+      <h2>{name}</h2>
+      <p>{tvShows}</p>
+    </div>
+  );
+};
 
 export default Card;
--- character-app/src/components/search-box/search-box.component.jsx
+++ character-app/src/components/search-box/search-box.component.jsx
@@ -1,21 +1,17 @@
-import { Component } from 'react';
 import '../search-box/search-box.style.css'
 
-class SearchBox extends Component {
-  render() {
-    return (
+const SearchBox = (props) => (
       <div>
         <i className={`fas fa-search searchicon`} />
         <input
-        className={`searchbox ${this.props.className}`}
+        className={`searchbox ${props.className}`}
         type="search"
-        placeholder={this.props.placeholder}
-        onChange={this.props.onChangeHandler}
+        placeholder={props.placeholder}
+        onChange={props.onChangeHandler}
         />
       </div>
       
     );
-  }
-}
+
 
 export default SearchBox;
